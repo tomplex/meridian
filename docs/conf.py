0854f0a15e7cb192dd43ddf777e74fed627d9380
Tom Caruso <carusot42@gmail.com>
2020-08-05 02:53:22 -0400

    mock import of rtree

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ extensions = [
     # 'sphinx.ext.mathjax',
 ]
 
+autodoc_mock_imports = ["rtree"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
